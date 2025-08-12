441a895324d11b522912dd42b6c0b9ab716fab3e
Raphael Gruber <raphael.gruber@siemens.com>
2025-08-12 17:47:50 +0200

    feat: add fish config

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+pyenv init - fish | source
+/home/rgruber/.local/bin/mise activate fish | source
+starship init fish | source
