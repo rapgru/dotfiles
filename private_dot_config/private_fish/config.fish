a56709f404357d1f77fd8df211d81959e28bc0a3
Raphael Gruber <raphael.gruber@rapgru.com>
2025-08-13 20:47:29 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,10 +1,34 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     starship init fish | source
+
+    alias dum1="du --max-depth=1"
+    alias dum2="du --max-depth=2"
+
+    alias .. "cd .."
+    alias cd.. "cd .."
+    alias ll "ls -la"
+    alias cat bat
+
+    export LESSOPEN="|/opt/homebrew/bin/lesspipe.sh %s"
+    set -x LESS_ADVANCED_PREPROCESSOR 1
+
+    fzf --fish | source
 end
 
-pyenv init - fish | source
-/home/rgruber/.local/bin/mise activate fish | source
+# Mise
+mise activate fish | source
 
-alias dum1="du --max-depth=1"
-alias dum2="du --max-depth=2"
+# Homebrew
+/opt/homebrew/bin/brew shellenv | source
+
+# Elan (Lean)
+if test -e $HOME/.elan/env
+    source $HOME/.elan/env
+end
+
+# GPG Stuff
+set -x GPG_TTY (tty)
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+
