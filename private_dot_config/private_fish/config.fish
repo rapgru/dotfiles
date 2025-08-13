5e52a27d7bfd7f3d41c5a73fda78333db5dedb30
Raphael Gruber <raphael.gruber@siemens.com>
2025-08-13 12:56:48 +0200

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    starship init fish | source
 end
 
 pyenv init - fish | source
 /home/rgruber/.local/bin/mise activate fish | source
-starship init fish | source
+
+alias dum1="du --max-depth=1"
+alias dum2="du --max-depth=2"
