11a6c8574db8fffb53a3341b6020d7aedd34cfd6
Raphael Gruber <raphael.gruber@cloudflight.io>
2026-09-09 18:40:06 +0200

    prompt

--- private_dot_config/private_fish/functions/executable_az-sub.fish
+++ private_dot_config/private_fish/functions/executable_az-sub.fish
@@ -1,3 +1,3 @@
 function az-sub
-  az account set --subscription (az account list --query '[].name' -o tsv | fzf)
+  az account set --subscription (az account list --query '[].name' -o tsv | fzf --prompt="az sub> " --height=40%)
 end
