2b6a6b7e6c377f5c22efe4b05531747180285732
Raphael Gruber <raphael.gruber@cloudflight.io>
2026-09-09 18:34:38 +0200

    neovim, tmux config

---
+++ private_dot_config/tmux/executable_sesh-connect.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+selected=$(
+  sesh list --hide-duplicates --icons | fzf \
+    --no-sort --ansi \
+    --border-label ' sesh ' \
+    --prompt '⚡  ' \
+    --header '  ^a all ^t tmux ^x zoxide ^d kill ^f find' \
+    --bind 'tab:down,btab:up' \
+    --bind 'ctrl-a:change-prompt(⚡  )+reload(sesh list --hide-duplicates --icons)' \
+    --bind 'ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)' \
+    --bind 'ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)' \
+    --bind 'ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)' \
+    --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --hide-duplicates --icons)' \
+    --preview-window 'right:55%' \
+    --preview 'sesh preview {}'
+) || true
+
+[ -n "$selected" ] && sesh connect "$selected"
